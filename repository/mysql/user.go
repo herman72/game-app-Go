d311b8aefbedd3742b47309e3c82b340005346f8
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-02-09 21:21:13 +0200

    :construction: user database

---
+++ repository/mysql/user.go
@@ -0,0 +1,13 @@
+package mysql
+
+import "game-app-go/entity"
+
+
+
+func(d MySQLDB)IsPhoneNumberUnique(phonenumber string)(bool, error){
+	return 
+}
+
+func(d MySQLDB)Register(u entity.User)(entity.User, error){
+	return
+}
