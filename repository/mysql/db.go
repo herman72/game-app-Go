ad188d913b9ce3da0aed29c4b1e9880da973b7e2
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-02-09 21:19:03 +0200

    :construction: created db page

---
+++ repository/mysql/db.go
@@ -0,0 +1,27 @@
+package mysql
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type MySQLDB struct {
+	db *sql.DB
+}
+
+func New()*MySQLDB {
+	db, err := sql.Open("mysql", "user:password@/dbname")
+
+	if err != nil {
+		panic(fmt.Errorf("can't open mysql db: %v",err))
+	}
+
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetConnMaxIdleTime(10)
+
+	return &MySQLDB{db}
+}
