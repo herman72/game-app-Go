2d764d47fcee32c95287fc4972b6673b0d3656c2
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-02-11 00:44:45 +0200

    :hammer: add new function

--- service/userservice/service.go
+++ service/userservice/service.go
@@ -24,6 +24,10 @@ type RegisterResponse struct {
 	User entity.User
 }
 
+func New(repo Repository) Service {
+	return Service{repo: repo}
+}
+
 func (s Service) Register(req RegisterRequest)(RegisterResponse, error){
 	// TODO: Verify phonenumber with verification code
 	if !phonenumber.IsValid(req.PhoneNumber){
