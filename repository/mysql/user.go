4c8d4ccfc721f7867ffcb230ded664d6d94e70ac
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-02-12 23:44:28 +0200

    :hammer: add getuserbyid function for login

--- repository/mysql/user.go
+++ repository/mysql/user.go
@@ -12,7 +12,7 @@ func (d *MySQLDB) IsPhoneNumberUnique(phoneNumber string) (bool, error) {
 	user := entity.User{}
 	var createdAt []uint8
 	row := d.db.QueryRow(`select * from users where phone_number = ?`, phoneNumber)
-	err := row.Scan(&user.ID, &user.Name, &user.PhoneNumber, &createdAt)
+	err := row.Scan(&user.ID, &user.Name, &user.PhoneNumber, &user.Password, &createdAt)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return true, nil
@@ -34,4 +34,19 @@ func(d *MySQLDB)Register(u entity.User)(entity.User, error){
 
 
 	return u, nil
+}
+
+
+func (d *MySQLDB) GetUserByPhoneNumber(phoneNumber string) (entity.User, bool, error) {
+	user := entity.User{}
+	var createdAt []uint8
+	row := d.db.QueryRow(`select * from users where phone_number = ?`, phoneNumber)
+	err := row.Scan(&user.ID, &user.Name, &user.PhoneNumber, &user.Password, &createdAt)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return entity.User{}, false, nil
+		}
+		return entity.User{}, false, fmt.Errorf("can't scan query result: %w", err)
+	}
+	return user, true, nil
 }
