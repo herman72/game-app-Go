fc9fe609d32d43d8bb493d8f9fa4dcb452463e76
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-02-12 23:45:08 +0200

    :hammer: add interface details and json style

--- service/userservice/service.go
+++ service/userservice/service.go
@@ -8,9 +8,10 @@ import (
 	"game-app-go/pkg/phonenumber"
 )
 
-type Repository interface{
+type Repository interface {
 	IsPhoneNumberUnique(phonenumber string) (bool, error)
-	Register(u entity.User)(entity.User, error)
+	Register(u entity.User) (entity.User, error)
+	GetUserByPhoneNumber(PhoneNumber string) (entity.User, bool, error)
 }
 
 type Service struct {
@@ -18,9 +19,9 @@ type Service struct {
 }
 
 type RegisterRequest struct {
-	Name string `json:"name"`
+	Name        string `json:"name"`
 	PhoneNumber string `json:"phone_number"`
-	Password string `json:"password"`
+	Password    string `json:"password"`
 }
 
 type RegisterResponse struct {
@@ -31,9 +32,9 @@ func New(repo Repository) Service {
 	return Service{repo: repo}
 }
 
-func (s Service) Register(req RegisterRequest)(RegisterResponse, error){
+func (s Service) Register(req RegisterRequest) (RegisterResponse, error) {
 	// TODO: Verify phonenumber with verification code
-	if !phonenumber.IsValid(req.PhoneNumber){
+	if !phonenumber.IsValid(req.PhoneNumber) {
 		return RegisterResponse{}, fmt.Errorf("phone number is not valid")
 	}
 
@@ -41,10 +42,10 @@ func (s Service) Register(req RegisterRequest)(RegisterResponse, error){
 		if err != nil {
 			return RegisterResponse{}, fmt.Errorf("unexpected Error %w", err)
 		}
-	
+
 		if !isUnique {
 			return RegisterResponse{}, fmt.Errorf("phone number is not unique")
-		}	
+		}
 	}
 
 	if len(req.Name) < 3 {
@@ -52,47 +53,54 @@ func (s Service) Register(req RegisterRequest)(RegisterResponse, error){
 	}
 
 	// TODO: validate password - use regex
-	if len(req.Password)<8{
+	if len(req.Password) < 8 {
 		return RegisterResponse{}, fmt.Errorf("password length should be greater then 8")
 	}
-	
+
 	// TODO: use bcycpt for hashing
-	
+
 	user := entity.User{
-		ID: 0,
+		ID:          0,
 		PhoneNumber: req.PhoneNumber,
-		Name: req.Name,
-		Password: getMD5Hash(req.Password),
+		Name:        req.Name,
+		Password:    getMD5Hash(req.Password),
 	}
 	createdUser, err := s.repo.Register(user)
 
-	if err != nil{
+	if err != nil {
 		return RegisterResponse{}, fmt.Errorf("unexpected error %w", err)
 	}
 
 	return RegisterResponse{
 		User: createdUser,
-
 	}, nil
-
-	
 
 }
 
 type LoginRequest struct {
-	PhoneNumber string
-	Password string
+	PhoneNumber string `json:"phone_number"`
+	Password    string	`json:"password"`
 }
 
 type LoginResponse struct {
-
 }
 
-func(s Service)Login(req LoginRequest)(LoginResponse, error){
-	panic("implement me")
+func (s Service) Login(req LoginRequest) (LoginResponse, error) {
+	// TODO - it would be better to user two separate method for existence check and getUserByPhoneNumber
+	user, exist, err := s.repo.GetUserByPhoneNumber(req.PhoneNumber)
+	if err != nil {
+		return LoginResponse{}, fmt.Errorf("unexpected error: %w", err)
+	}
+	if !exist {
+		return LoginResponse{}, fmt.Errorf("username or password isn't correct")
+	}
+	if user.Password != getMD5Hash(req.Password) {
+		return LoginResponse{}, fmt.Errorf("username or password isn't correct")
+	}
+	return LoginResponse{}, nil
 }
 
-func getMD5Hash(text string)string{
+func getMD5Hash(text string) string {
 	hash := md5.Sum([]byte(text))
 	return hex.EncodeToString(hash[:])
 }
