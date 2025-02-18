359dbd2681f081e4577317e5e39ffdbc2cf8e2bd
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-02-18 12:10:41 +0200

    :construction: added serve user login

--- delivery/httpserver/user_service.go
+++ delivery/httpserver/user_service.go
@@ -24,3 +24,48 @@ func (s Server)userRegister(c echo.Context)error{
 
 	return c.JSON(http.StatusCreated, Resp)
 }
+
+func (s Server)userLogin(c echo.Context) error {
+
+	
+	var req userservice.LoginRequest
+
+	if err := c.Bind(&req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest)
+	}
+	
+	resp, err := s.userSvc.Login(req)
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		
+	}
+
+	return c.JSON(http.StatusOK, resp)
+
+}
+
+func (s Server)userProfile(c echo.Context)error{
+	// 	// sessionID := req.Header.Get("SessionID")
+// 	// TODO: Validate sessionid by database and get user id
+
+	// validate jwt token and retrive userID from pyload
+
+	
+	authToken := c.Request().Header.Get("Authorization")	
+	claims, err := s.authSvc.ParseToken(authToken)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusUnauthorized, err.Error())
+	}
+	
+
+	resp, err := s.userSvc.Profile(userservice.ProfileRequest{UserID: claims.UserID})
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, resp)
+
+
+}
