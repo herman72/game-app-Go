430691a5e2fff06cbdb8d752bb7e1a4a83a3d379
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-02-21 17:39:40 +0200

    :construction: add validator to the user service

--- delivery/httpserver/user_service.go
+++ delivery/httpserver/user_service.go
@@ -1,6 +1,7 @@
 package httpserver
 
 import (
+	"game-app-go/dto"
 	"game-app-go/pkg/errmsg/httpmsg"
 	"game-app-go/service/userservice"
 	"net/http"
@@ -8,13 +9,20 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func (s Server)userRegister(c echo.Context)error{
-	
+func (s Server) userRegister(c echo.Context) error {
 
-	var uReq userservice.RegisterRequest
+	var uReq dto.RegisterRequest
 
 	if err := c.Bind(&uReq); err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest)
+	}
+	if err, fieldErrors := s.userValidator.ValidateRegisterRequest(uReq); err != nil {
+
+		msg, code := httpmsg.Error(err)
+		return c.JSON(code, echo.Map{
+			"message": msg,
+			"errors":  fieldErrors,
+		})
 	}
 
 	Resp, err := s.userSvc.Register(uReq)
@@ -26,39 +34,36 @@ func (s Server)userRegister(c echo.Context)error{
 	return c.JSON(http.StatusCreated, Resp)
 }
 
-func (s Server)userLogin(c echo.Context) error {
+func (s Server) userLogin(c echo.Context) error {
 
-	
 	var req userservice.LoginRequest
 
 	if err := c.Bind(&req); err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest)
 	}
-	
+
 	resp, err := s.userSvc.Login(req)
 
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-		
+
 	}
 
 	return c.JSON(http.StatusOK, resp)
 
 }
 
-func (s Server)userProfile(c echo.Context)error{
+func (s Server) userProfile(c echo.Context) error {
 	// 	// sessionID := req.Header.Get("SessionID")
-// 	// TODO: Validate sessionid by database and get user id
+	// 	// TODO: Validate sessionid by database and get user id
 
 	// validate jwt token and retrive userID from pyload
 
-	
-	authToken := c.Request().Header.Get("Authorization")	
+	authToken := c.Request().Header.Get("Authorization")
 	claims, err := s.authSvc.ParseToken(authToken)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusUnauthorized, err.Error())
 	}
-	
 
 	resp, err := s.userSvc.Profile(userservice.ProfileRequest{UserID: claims.UserID})
 
@@ -68,6 +73,5 @@ func (s Server)userProfile(c echo.Context)error{
 	}
 
 	return c.JSON(http.StatusOK, resp)
-
 
 }
