074eeaaadf5cc731bc943921e112480d65bf7625
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-04-28 20:07:13 +0300

    :recycle: addd usr

---
+++ service/userservice/register.go
@@ -0,0 +1,32 @@
+package userservice
+
+import (
+	"fmt"
+	"game-app-go/dto"
+	"game-app-go/entity"
+)
+
+func (s Service) Register(req dto.RegisterRequest) (dto.RegisterResponse, error) {
+	// TODO: Verify phonenumber with verification code
+
+	// TODO: use bcycpt for hashing
+
+	user := entity.User{
+		ID:          0,
+		PhoneNumber: req.PhoneNumber,
+		Name:        req.Name,
+		Password:    getMD5Hash(req.Password),
+	}
+	createdUser, err := s.repo.Register(user)
+
+	if err != nil {
+		return dto.RegisterResponse{}, fmt.Errorf("unexpected error %w", err)
+	}
+
+	return dto.RegisterResponse{User: dto.UserInfo{
+		ID: createdUser.ID,
+		PhoneNumber: createdUser.PhoneNumber,
+		Name: createdUser.Name,
+	}}, nil
+
+}
