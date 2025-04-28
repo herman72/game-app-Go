eda6b5beba3c31530b5cdee5e613e05106aee4e9
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-04-28 20:08:06 +0300

    :recycle: solve minor issue about import

--- delivery/httpserver/user_service.go
+++ delivery/httpserver/user_service.go
@@ -3,7 +3,6 @@ package httpserver
 import (
 	"game-app-go/dto"
 	"game-app-go/pkg/errmsg/httpmsg"
-	"game-app-go/service/userservice"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -36,7 +35,7 @@ func (s Server) userRegister(c echo.Context) error {
 
 func (s Server) userLogin(c echo.Context) error {
 
-	var req userservice.LoginRequest
+	var req dto.LoginRequest
 
 	if err := c.Bind(&req); err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest)
@@ -65,7 +64,7 @@ func (s Server) userProfile(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusUnauthorized, err.Error())
 	}
 
-	resp, err := s.userSvc.Profile(userservice.ProfileRequest{UserID: claims.UserID})
+	resp, err := s.userSvc.Profile(dto.ProfileRequest{UserID: claims.UserID})
 
 	if err != nil {
 		msg, code := httpmsg.Error(err)
