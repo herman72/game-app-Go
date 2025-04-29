dfa3108d089d9949bc8ae91229fb397dd142e2d4
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-04-29 18:09:09 +0300

    :recycle: change get user by phone method

--- repository/mysql/user.go
+++ repository/mysql/user.go
@@ -40,20 +40,23 @@ func(d *MySQLDB)Register(u entity.User)(entity.User, error){
 }
 
 
-func (d *MySQLDB) GetUserByPhoneNumber(phoneNumber string) (entity.User, bool, error) {
+func (d *MySQLDB) GetUserByPhoneNumber(phoneNumber string) (entity.User, error) {
 	const op = "mysql.GetUserByPhoneNumber"
 	row := d.db.QueryRow(`select * from users where phone_number = ?`, phoneNumber)
 	user,err := scanUser(row)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			return entity.User{}, false, nil
+			return entity.User{}, richerror.New(op).WithError(err).
+			WithMessage(errmsg.ErrorMsgNotFound).WithKind(richerror.KindNotFound)
 		}
+		fmt.Println("Unexpected error: ", err)
+		// TODO: Log unexpected error for better observibility
 		// fmt.Errorf("can't scan query result: %w", err)
-		return entity.User{}, false, richerror.New(op).WithError(err).
+		return entity.User{}, richerror.New(op).WithError(err).
 			WithMessage(errmsg.ErrorMsgCantScanQueryResult).WithKind(richerror.KindUnexpected)
 		
 	}
-	return user, true, nil
+	return user, nil
 }
 
 func (d *MySQLDB) GetUserByID(userID uint) (entity.User, error) {
