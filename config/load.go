9edc58da1c3e89da551f1b2e8026c0a9da977cff
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-05-12 16:01:50 +0300

    :hammer: added conf struct

--- config/load.go
+++ config/load.go
@@ -1,26 +1,40 @@
 package config
 
 import (
-	"github.com/knadh/koanf/providers/confmap"
-	"github.com/knadh/koanf/v2"
-	"github.com/knadh/koanf/providers/file"
-	"github.com/knadh/koanf/providers/env"
 	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/confmap"
+	"github.com/knadh/koanf/providers/env"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/v2"
+	"strings"
 )
 
-func Load() *Config {
+func Load(configPath string) Config {
+	// Global koanf instance. Use "." as the key path delimiter. This can be "/" or any character.
 	var k = koanf.New(".")
-	k.Load(confmap.Provider(map[string]interface{}{
-		"aut.refresh_subject": RefreshTokenSubject,
-		"aut.access_subject":  AccessTokenSubject,
-	}, "."), nil)
 
-	k.Load(file.Provider("config.yaml"), yaml.Parser())
+	// Load default values using the confmap provider.
+	// We provide a flat map with the "." delimiter.
+	// A nested map can be loaded by setting the delimiter to an empty string "".
+	k.Load(confmap.Provider(defaultConfig, "."), nil)
+
+	// Load YAML config and merge into the previously loaded config (because we can).
+	k.Load(file.Provider(configPath), yaml.Parser())
+
 	k.Load(env.Provider("GAMEAPP_", ".", func(s string) string {
-		return 	}), nil)
+		str := strings.Replace(strings.ToLower(
+			strings.TrimPrefix(s, "GAMEAPP_")), "_", ".", -1)
+
+		// TODO - this is just a workaround
+		// for multiword items such as "sign_key" that we should use like "GAMEAPP_AUTH_SIGN__KEY"
+		// find a better solution if needed..
+		return strings.Replace(str, "..", "_", -1)
+	}), nil)
+
 	var cfg Config
 	if err := k.Unmarshal("", &cfg); err != nil {
 		panic(err)
 	}
-	return &cfg
+
+	return cfg
 }
